ad21c9a722d5d12f2779095ab5725628f5566e67
TheDemon12345678910 <anehoimark@gmail.com>
2023-09-22 11:31:33 +0200

    Adding The Readingfile

--- Program.cs
+++ Program.cs
@@ -1,6 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello there.\n it ");
-//Getting an input from the user
 
+using encryptionPassPhrase;
+
+
+Console.WriteLine("Hello there.\n it seems you have some secret stuff you want to either encrypt or decrypt");
+//Getting an input from the user
+Console.WriteLine("What would you like to do right now?\nA) Read a secret file\nB) Write a secret file");
+string choice = Console.ReadLine();
+ReadingFile readingFile = new ReadingFile();
+EncryptingAndCreatingFile encryptingAndCreatingFile = new EncryptingAndCreatingFile();
 
 //Sending the user to the wanted page
+
+if (choice != null)
+{
+    if (choice == "A")
+    {        
+        Console.WriteLine("You have chosen to read an already existing file");
+        readingFile.readingFile();
+    }
+
+    if (choice == "B")
+    {
+        Console.WriteLine("You have chosen to write a new file");
+        encryptingAndCreatingFile.encryptingAndCreatingFile();
+    }
+}
--- ReadingFile.cs
+++ ReadingFile.cs
@@ -2,5 +2,10 @@
 
 public class ReadingFile
 {
-    
+    public void readingFile()
+    {
+        Console.WriteLine("So You want to Read a File i see.");
+    }
+
+
 }
